e24a3963c53d79d7a768b462ab0fb314ea15528c
lyh-ADT <1043970238@qq.com>
2021-09-21 12:39:52 +0800

    feat: 完成Predicate

--- src/java/simpledb/execution/Predicate.java
+++ src/java/simpledb/execution/Predicate.java
@@ -17,11 +17,9 @@ public class Predicate implements Serializable {
         EQUALS, GREATER_THAN, LESS_THAN, LESS_THAN_OR_EQ, GREATER_THAN_OR_EQ, LIKE, NOT_EQUALS;
 
         /**
-         * Interface to access operations by integer value for command-line
-         * convenience.
+         * Interface to access operations by integer value for command-line convenience.
          * 
-         * @param i
-         *            a valid integer Op index
+         * @param i a valid integer Op index
          */
         public static Op getOp(int i) {
             return values()[i];
@@ -46,69 +44,59 @@ public class Predicate implements Serializable {
         }
 
     }
-    
+
+    private final int fieldIndex;
+    private final Op operation;
+    private final Field value;
+
     /**
      * Constructor.
      * 
-     * @param field
-     *            field number of passed in tuples to compare against.
-     * @param op
-     *            operation to use for comparison
-     * @param operand
-     *            field value to compare passed in tuples to
+     * @param field   field number of passed in tuples to compare against.
+     * @param op      operation to use for comparison
+     * @param operand field value to compare passed in tuples to
      */
     public Predicate(int field, Op op, Field operand) {
-        // some code goes here
+        this.fieldIndex = field;
+        this.operation = op;
+        this.value = operand;
     }
 
     /**
      * @return the field number
      */
-    public int getField()
-    {
-        // some code goes here
-        return -1;
+    public int getField() {
+        return fieldIndex;
     }
 
     /**
      * @return the operator
      */
-    public Op getOp()
-    {
-        // some code goes here
-        return null;
-    }
-    
-    /**
-     * @return the operand
-     */
-    public Field getOperand()
-    {
-        // some code goes here
-        return null;
-    }
-    
-    /**
-     * Compares the field number of t specified in the constructor to the
-     * operand field specified in the constructor using the operator specific in
-     * the constructor. The comparison can be made through Field's compare
-     * method.
-     * 
-     * @param t
-     *            The tuple to compare against
-     * @return true if the comparison is true, false otherwise.
-     */
-    public boolean filter(Tuple t) {
-        // some code goes here
-        return false;
+    public Op getOp() {
+        return operation;
     }
 
     /**
-     * Returns something useful, like "f = field_id op = op_string operand =
-     * operand_string"
+     * @return the operand
      */
+    public Field getOperand() {
+        return value;
+    }
+
+    /**
+     * Compares the field number of t specified in the constructor to the operand
+     * field specified in the constructor using the operator specific in the
+     * constructor. The comparison can be made through Field's compare method.
+     * 
+     * @param t The tuple to compare against
+     * @return true if the comparison is true, false otherwise.
+     */
+    public boolean filter(Tuple t) {
+        return t.getField(fieldIndex).compare(operation, value);
+    }
+
+    @Override
     public String toString() {
-        // some code goes here
-        return "";
+        return "Predicate [fieldIndex=" + fieldIndex + ", operation=" + operation + ", value=" + value + "]";
     }
 }
